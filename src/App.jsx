5fb22e8cc8cb95fd5bdc0ac970c5b3f56c74fabd
Zulkifli Firdausi <65410126+Zulkifli70@users.noreply.github.com>
2026-07-13 22:21:10 +0700

    Create About section (not done)

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Header from "./components/Header";
 import BaseLayout from "./layout/BaseSections";
 import Hero from "./sections/Hero";
 import "./App.css";
+import About from "./sections/About";
 
 function App() {
   return (
     <BaseLayout>
       <Header />
       <Hero />
+      <About />
     </BaseLayout>
   );
 }
---
+++ src/sections/About.jsx
@@ -0,0 +1,9 @@
+import Section from "../layout/Section";
+
+export default function About() {
+  return (
+    <Section>
+      <h1>INI ABOUT</h1>
+    </Section>
+  );
+}
