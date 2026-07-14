f3b9e461f152806034879c8ee1a45086476d8ba6
Zulkifli Firdausi <65410126+Zulkifli70@users.noreply.github.com>
2026-07-14 14:23:32 +0700

    selesai itu about section untuk sekarang

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,11 +1,58 @@
 import Section from "../layout/Section";
+import { useRef } from "react";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/SplitText";
+import { ScrambleTextPlugin } from "gsap/ScrambleTextPlugin";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(SplitText, ScrollTrigger, ScrambleTextPlugin);
 
 export default function About() {
+  const containerRef = useRef(null);
+
+  useGSAP(
+    () => {
+      const split = new SplitText(".para", { type: "words" });
+
+      gsap.from(split.words, {
+        x: 150,
+        opacity: 0,
+        duration: 2,
+        ease: "power4",
+        stagger: 0.05,
+        scrollTrigger: {
+          trigger: ".para",
+          start: "top center",
+          toggleActions: "play none none reverse",
+        },
+      });
+
+      gsap.to(".about", {
+        duration: 3,
+        scrambleText: {
+          text: "About Me",
+          chars: "lowercase",
+          rightToLeft: false,
+          tweenLength: false,
+        },
+        scrollTrigger: {
+          trigger: ".about",
+          start: "center center",
+          toggleActions: "play none none reverse",
+        },
+      });
+
+      return () => split.revert();
+    },
+    { scope: containerRef },
+  );
+
   return (
-    <Section>
-      <div className="w-full h-screen grid items-center pt-20">
-        <h2 className="font-neuton text-9xl">About Me</h2>
-        <p className="font-neuton text-7xl w-2/3">
+    <Section ref={containerRef}>
+      <div className="w-full h-screen flex flex-col justify-around gap-15 py-20">
+        <h2 className="about font-neuton text-9xl"></h2>
+        <p className="para self-end font-neuton text-7xl w-2/3">
           Hello, I'm Zulk, an indonesian frontend developer who enjoys turning
           ideas into resfponsive interaces that feel modern and easy to use. I
           care about the small details that make a product smoother, from layout
