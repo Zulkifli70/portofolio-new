84282e5adfb201214dca994d2b9d756bf135d341
Zulkifli Firdausi <65410126+Zulkifli70@users.noreply.github.com>
2026-07-16 16:37:22 +0700

    lupa juga disini cuy

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -49,7 +49,7 @@ export default function About() {
   );
 
   return (
-    <Section ref={containerRef} className="overflow-x-clip px-6">
+    <Section id="about" ref={containerRef} className="overflow-x-clip px-6">
       <div className="relative w-full min-h-screen overflow-hidden flex flex-col gap-15 py-20">
         <h2 className="about font-neuton font-bold text-6xl md:text-9xl border-b border-b-gray-400"></h2>
         <p className="para md:self-end absolute top-1/3 font-neuton text-2xl md:text-7xl md:w-2/3">
