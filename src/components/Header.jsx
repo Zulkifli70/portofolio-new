5878d8afa772abd8efdacd46ddd9ed7a4cf6c95d
Zulkifli Firdausi <65410126+Zulkifli70@users.noreply.github.com>
2026-07-16 17:28:53 +0700

    kelar nambahin scroll to section di header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,40 @@
+import { useRef } from "react";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+
 export default function Header() {
+  const navRef = useRef(null);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    const target = e.currentTarget.getAttribute("href"); // contoh: "#about"
+
+    // Ambil instance ScrollSmoother yang sudah aktif dari SmoothScrollPortfolio
+    const smoother = ScrollSmoother.get();
+
+    if (smoother) {
+      smoother.scrollTo(target, true, "top top");
+    }
+  };
+
   return (
     <header className="header w-max-7xl flex items-center justify-between py-5 px-6 border-gray-200 fixed top-0 left-0 right-0 z-20">
-      <a href="#" className="logo text-3xl text-gray-900 font-neuton font-bold">
+      <a href="#" className="logo text-3xl font-neuton font-bold">
         Zulk
       </a>
 
-      <nav className="nav flex gap-6">
-        <a href="#about" className="text-sm text-gray-900 hover:opacity-60">
+      <nav ref={navRef} className="nav flex gap-6">
+        <a
+          href="#about"
+          onClick={handleClick}
+          className="text-xl text-gray-900 hover:opacity-60"
+        >
           About
         </a>
-        <a href="#projects" className="text-sm text-gray-900 hover:opacity-60">
+        <a
+          href="#projects"
+          onClick={handleClick}
+          className="text-xl text-gray-900 hover:opacity-60"
+        >
           Projects
         </a>
       </nav>
